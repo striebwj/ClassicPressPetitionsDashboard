1efe96481cd14dff1aca37d2a37d0203af498da8
John Alarcon <johnalarcon@users.noreply.github.com>
2018-11-21 15:05:49 -0800

    Update ClassicPressPetitionsDashboard.php
    Remove WordPress events from dashboard widgets.

--- ClassicPressPetitionsDashboard.php
+++ ClassicPressPetitionsDashboard.php
@@ -23,40 +23,49 @@ class ClassicPressPetitionsDashboard {
 
 	public function start() {
 		add_action( 'wp_dashboard_setup', array( $this, 'cp_add_dashboard_widgets' ) );
+		add_action( 'wp_dashboard_setup', array( $this, 'remove_wordpress_events' ) );
 	}
+
 	/**
 	 * Add a widget to the dashboard.
 	 */
 	public function cp_add_dashboard_widgets() {
-	
 		wp_add_dashboard_widget(
 			'cp_petitions_dashboard_widget', // Widget slug.
 			'ClassicPress Petitions', // Title.
 			array( $this, 'cp_petitions_dashboard_widget_cb' ) // Display function.
-		);	
-			
+		);
+
 	}
-		
+
 	/**
 	 * Callback function to output the contents of our Dashboard Widget.
 	 */
 	public function cp_petitions_dashboard_widget_cb() {
 		$string = file_get_contents("https://petitions.classicpress.net/api/v1/posts?f=most-wanted");
 		$json = json_decode($string, true);
-	
-		foreach( $json as $key => $value ) { 
+
+		foreach( $json as $key => $value ) {
 		?>
 			<ul class="cp_petitions">
 				<li><a href="https://petitions.classicpress.net/posts/<?php echo $value['number'] . '/' . $value['slug']; ?>" target="_blank"><?php echo $value['title']; ?></a> by <?php echo $value['user']['name']; ?><br >
 				<!-- 	<?php echo $value['description']; ?> -->
 					<strong>Up Votes:</strong> <?php echo $value['votesCount']; ?>
-					<strong>Status:</strong> <?php echo $value['status']; ?> 
-					<strong>Created:</strong> <?php echo date('d-M-Y',strtotime($value['createdAt'])); ?> 
+					<strong>Status:</strong> <?php echo $value['status']; ?>
+					<strong>Created:</strong> <?php echo date('d-M-Y',strtotime($value['createdAt'])); ?>
 				</li>
 			</ul>
 		<?php
 		}
 	}
+
+	/**
+	 * Remove WordPress events.
+	 */
+	public function remove_wordpress_events() {
+		remove_meta_box( 'dashboard_primary', 'dashboard', 'side' );
+	}
+
 }
 
 $run = new ClassicPressPetitionsDashboard;
