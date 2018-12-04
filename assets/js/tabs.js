a0ac47d17e87345a05c505fb042099f7e1dbe82a
Laurence Bahiirwa <laurence@mediauganda.com>
2018-12-04 11:10:15 +0300

    JS

--- assets/js/tabs.js
+++ assets/js/tabs.js
@@ -1,18 +1,18 @@
 function showTable(evt, type) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(type).style.display = "block";
-    evt.currentTarget.className += " active";
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(type).style.display = "block";
+  evt.currentTarget.className += " active";
 }
 
-window.addEventListener("load", function(){
-    var trending = document.getElementsByClassName('trending');
-    trending[0].click();
+window.addEventListener("load", function() {
+  var trending = document.getElementsByClassName("trending");
+  trending[0].click();
 });
